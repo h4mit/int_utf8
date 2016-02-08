2e598510ef1103818eeb6be4b01e522ea50271bf
Hamid Taheri <h4mid.taheri@gmail.com>
2016-02-08 10:19:50 +0330

    add persian utf8 number to integer parser

---
+++ UTF8ToInt.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: H4mit
+ * Date: 19/10/2015
+ * Time: 10:38 AM
+ */
+
+namespace Zaeim\ContactBundle\Services;
+
+
+class UTF8ToInt
+{
+    /**
+     * @param $utf8
+     * @return int
+     */
+    public static function utf8_to_int($utf8){
+        $len = strlen($utf8);
+        $number = 0;
+        for($i=1;$i<=floor($len/2);++$i)
+        {
+            $n = UTF8ToInt::UTF8NumberToInteger(substr($utf8, ($i - 1) * 2, 2));
+            $number = ($number * 10 ) + $n;
+        }
+        return $number;
+    }
+
+    /**
+     * @param one utf8 number
+     * @return one integer digit number
+     */
+    private static function UTF8NumberToInteger($utf8Number)
+    {
+        switch($utf8Number)
+        {
+            case '۰':
+                return 0;
+                break;
+            case '۱':
+                return 1;
+                break;
+            case '۲':
+                return 2;
+                break;
+            case '۳':
+                return 3;
+                break;
+            case '۴':
+                return 4;
+                break;
+            case '۵':
+                return 5;
+                break;
+            case '۶':
+                return 6;
+                break;
+            case '۷':
+                return 7;
+                break;
+            case '۸':
+                return 8;
+                break;
+            case '۹':
+                return 9;
+                break;
+            default:
+                return $utf8Number;
+        }
+    }
+}
